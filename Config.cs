c38142fd634cf8e9994cc403485f57997e169380
amacapozzi <105817728+amacapozzi@users.noreply.github.com>
2024-10-04 12:16:38 -0300

    Remove API KEY

--- Config.cs
+++ Config.cs
@@ -2,6 +2,6 @@
 {
     internal class Config
     {
-        public static readonly string FILE_IO_APILEY = "773ef17d-3d54-453b-b7d7-c23415ac8b68";
+        public static readonly string FILE_IO_APILEY = "";
     }
 }
